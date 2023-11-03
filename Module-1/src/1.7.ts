cbff1125c29e2a207ebbe986cdebcf0d46498e0a
unknown <mejbacsedu@gmail.com>
2023-11-03 11:47:05 +0600

    destructuring

--- Module-1/src/1.7.ts
+++ Module-1/src/1.7.ts
@@ -1,5 +1,9 @@
-{
+{ // Scond bracket used for not this program not working will global scope
  const poorUser = {
-    
+    name: "Biden" 
  }
 }
+
+// Spread Operator
+// rest operators 
+// destructing operators
---
+++ Module-1/src/destructuring1.8.ts
@@ -0,0 +1,21 @@
+{
+
+// destructuring
+
+const user = {
+    id: 365,
+    name: {
+        firstName: 'John',
+        middleName: 'kabir',
+        lastName: 'Khan',
+    },
+    contactNo: '017000000',
+    address:'Bangladesh',
+    
+}
+
+
+const {contactNo, 
+    name:{middleName : midName} // name alias, you can not use identifier as middleName:string here.
+} = user;
+}

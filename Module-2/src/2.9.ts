4205608ca65904fbc9d86d3a130442cc8bdc7ffe
unknown <mejbacsedu@gmail.com>
2023-11-05 12:50:03 +0600

    contional type

---
+++ Module-2/src/2.9.ts
@@ -0,0 +1,23 @@
+{
+    //conditional type
+    // ekta types man onno typer upor depended
+    type A1 = number;
+    type B1 = undefined;
+    type X = A1 extends null ? true : false;
+    type Y = A1 extends null ? true : B1 extends undefined ? true : false; 
+    
+
+    type sheikh ={
+        bike: string;
+        car: string;
+        ship: string;
+        plane: string;
+    }
+
+    type CheckVehicle<T> = T extends keyof sheikh ? true : false;
+
+    type HasShip = CheckVehicle<"ship">
+    type HasTractor = CheckVehicle<"tractor">
+    type HasPlane = CheckVehicle<"plane">
+    //
+}

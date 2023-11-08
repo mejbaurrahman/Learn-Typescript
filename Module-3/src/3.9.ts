31b0696664baf3ef2f8f15f33ab781bb6079a09b
unknown <mejbacsedu@gmail.com>
2023-11-08 15:35:13 +0600

    Abstract and Interface

---
+++ Module-3/src/3.9.ts
@@ -0,0 +1,67 @@
+{
+    // abstraction  1. interface, 2. abstract class use kore
+
+    interface Vehicle1{
+        // name: string;
+        // model: number;
+
+        //idea or just declare function
+        startEngine(): void;
+        stopEngine(): void;
+        move(): void;
+
+    }
+
+    class Car implements Vehicle1{
+        // implement the function interface
+        startEngine(): void {
+         console.log("Starting the Engine")
+        }
+        stopEngine(): void {
+            console.log("Stopping the Engine")
+        }
+        move(): void {
+            console.log("Move the car")
+        }
+        test(){
+            console.log("I am just testing");
+        }
+
+
+    }
+    // const vehicle1: Vehicle1 ={
+    //     name: "Toyota",
+    //     model: 2000
+    // }
+
+    const Toyota = new Car();
+    Toyota.startEngine();
+    Toyota.stopEngine();
+    Toyota.move();
+    // 
+
+    // abstract class 
+    // ...........................................................................
+    // idea
+   abstract class Car2 {
+     abstract startEngine(): void;
+      abstract stopEngine(): void ;
+      abstract move(): void ;
+           test(){
+               console.log("I am just testing");
+           }
+    }
+
+    class ToyotaCar extends Car2{
+        startEngine(): void {
+            console.log("Starting the Engine")
+           }
+           stopEngine(): void {
+               console.log("Stopping the Engine")
+           }
+           move(): void {
+               console.log("Move the car")
+           }
+    }
+
+}
